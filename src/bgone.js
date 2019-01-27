ae640d59330aa2efb54637b34fac021b2ae390d8
DaveP <dave@wearecrystal.uk>
2019-01-27 18:47:24 +0000

    Tidy

--- src/bgone.js
+++ src/bgone.js
@@ -10,25 +10,23 @@ bot.login(process.env.TOKEN).catch(e => core.log.error(e));
 bot.on('ready', () => core.log.info('ready'));
 bot.on('error', e => core.log.error(e));
 bot.on('warn', w => core.log.warn(w));
-
 bot.on('message', msg => {
-  if (!msg.content.startsWith('bgone') || msg.author.bot) return;
+  if (msg.content.startsWith('bgone') && !msg.author.bot) handle(msg);
+});
 
+function handle(msg) {
   const args = msg.content
     .slice(5)
     .trim()
-    .split(/ +/g);
+    .split(' ');
 
   const cmd = msg.attachments.array().length
     ? 'image'
     : args.shift().toLowerCase();
-
-  // console.log('args:', args);
-  // console.log('cmd:', cmd);
 
   let command = commands.find(({ trigger }) =>
     trigger instanceof RegExp ? trigger.test(cmd) : trigger == cmd
   );
 
   if (command) command.run(args);
-});
+}
