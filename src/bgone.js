374afcf46dd671784ee8dc04686a190781008baf
DaveP <dave@wearecrystal.uk>
2019-01-27 16:29:59 +0000

    Index file with the usual Discord shit

--- src/bgone.js
+++ src/bgone.js
@@ -1,3 +1,23 @@
 import dotenv from 'dotenv/config';
+import core from './core';
+import { Client } from 'discord.js';
 
-console.log(process.env.TOKEN);
+const bot = new Client();
+
+bot.login(process.env.TOKEN);
+
+bot.on('ready', () => core.log.info('ready'));
+
+bot.on('message', msg => {
+  if (!msg.content.startsWith('bgone') || msg.author.bot) return;
+
+  const args = msg.content
+    .slice(5)
+    .trim()
+    .split(/ +/g);
+
+  const cmd = args.shift().toLowerCase();
+
+  console.log(args);
+  console.log(cmd);
+});
