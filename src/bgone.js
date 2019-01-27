b13af057fb81e6cafa2be403cfdf311c4abf9a6e
DaveP <dave@wearecrystal.uk>
2019-01-27 18:57:18 +0000

    Handler module

--- src/bgone.js
+++ src/bgone.js
@@ -1,6 +1,6 @@
+import handler from './modules/handler';
 import { Client } from 'discord.js';
 import dotenv from 'dotenv/config';
-import commands from './commands';
 import core from './modules/core';
 
 const bot = new Client();
@@ -10,23 +10,4 @@ bot.login(process.env.TOKEN).catch(e => core.log.error(e));
 bot.on('ready', () => core.log.info('ready'));
 bot.on('error', e => core.log.error(e));
 bot.on('warn', w => core.log.warn(w));
-bot.on('message', msg => {
-  if (msg.content.startsWith('bgone') && !msg.author.bot) handle(msg);
-});
-
-function handle(msg) {
-  const args = msg.content
-    .slice(5)
-    .trim()
-    .split(' ');
-
-  const cmd = msg.attachments.array().length
-    ? 'image'
-    : args.shift().toLowerCase();
-
-  let command = commands.find(({ trigger }) =>
-    trigger instanceof RegExp ? trigger.test(cmd) : trigger == cmd
-  );
-
-  if (command) command.run(args);
-}
+bot.on('message', m => handler(m));
---
+++ src/modules/handler.js
@@ -0,0 +1,20 @@
+import commands from '../commands';
+
+export default function handle(msg) {
+  if (!msg.content.startsWith('bgone') || msg.author.bot) return;
+
+  const args = msg.content
+    .slice(5)
+    .trim()
+    .split(' ');
+
+  const cmd = msg.attachments.array().length
+    ? 'image'
+    : args.shift().toLowerCase();
+
+  let command = commands.find(({ trigger }) =>
+    trigger instanceof RegExp ? trigger.test(cmd) : trigger == cmd
+  );
+
+  if (command) command.run(args);
+}
