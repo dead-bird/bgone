cf305e759110b254d41225476a417f5038317e1f
DaveP <dave@wearecrystal.uk>
2019-01-27 18:40:36 +0000

    Watch some events

--- src/bgone.js
+++ src/bgone.js
@@ -8,6 +8,8 @@ const bot = new Client();
 bot.login(process.env.TOKEN).catch(e => core.log.error(e));
 
 bot.on('ready', () => core.log.info('ready'));
+bot.on('error', e => core.log.error(e));
+bot.on('warn', w => core.log.warn(w));
 
 bot.on('message', msg => {
   if (!msg.content.startsWith('bgone') || msg.author.bot) return;
