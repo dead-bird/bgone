6fb6696ded50524aaac8fb8bcaba6cd1dda89f40
DaveP <dave@wearecrystal.uk>
2019-01-28 23:22:53 +0000

    avatar args

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -3,5 +3,13 @@ import Command from '../modules/Command';
 export default new Command({
   name: 'Avatar',
   trigger: 'avatar',
-  args: [],
+  args: [
+    {
+      name: '@user',
+      required: true,
+      describe: 'Get avatar from tagged user',
+    },
+  ],
+
+  // Get avatar from user or tagged user pass URL to Jimp
 });
