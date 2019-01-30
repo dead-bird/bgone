3d5473912b24418249cd6334f8cfb6054bcff6d8
DaveP <dave@wearecrystal.uk>
2019-01-30 23:23:44 +0000

    optional arg merge

--- src/modules/Command.js
+++ src/modules/Command.js
@@ -6,30 +6,32 @@ export default class Command {
 
     this.trigger = options.trigger;
 
-    this.args = [
-      ...options.args,
-      {
-        name: 'fit',
-        required: false,
-        describe: 'Change how your image is processed. (defaults to contain)',
-        options: [
-          {
-            name: 'contain',
-            describe: 'some parts of the image may be letter boxed',
-          },
-          {
-            name: 'cover',
-            describe: 'some parts of the image may be clipped',
-          },
-        ],
-      },
-      {
-        name: 'ratio',
-        required: false,
-        options: [],
-        describe: 'currently unsused',
-      },
-    ];
+    if (options.args) {
+      this.args = [
+        ...options.args,
+        {
+          name: 'fit',
+          required: false,
+          describe: 'Change how your image is processed. (defaults to contain)',
+          options: [
+            {
+              name: 'contain',
+              describe: 'some parts of the image may be letter boxed',
+            },
+            {
+              name: 'cover',
+              describe: 'some parts of the image may be clipped',
+            },
+          ],
+        },
+        {
+          name: 'ratio',
+          required: false,
+          options: [],
+          describe: 'currently unsused',
+        },
+      ];
+    }
 
     this.run = options.run
       ? options.run
