b6878b5aca192803875a430bf081044dcf196a06
DaveP <dave@wearecrystal.uk>
2019-01-30 23:23:36 +0000

    Include help

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,6 +1,7 @@
 import avatar from './avatar';
 import recent from './recent';
 import image from './image';
+import help from './help';
 import url from './url';
 
-export default [avatar, recent, image, url];
+export default [avatar, recent, image, help, url];
