11648a6627deb2aaee015a32ef8e7b8217bb1723
DaveP <dave@wearecrystal.uk>
2019-01-31 23:29:42 +0000

    Tidier image processor

--- src/modules/process.js
+++ src/modules/process.js
@@ -1,35 +1,28 @@
 import core from './core';
 import Jimp from 'jimp';
 
-export default async function(url, constraint = 'contain', ratio) {
-  const allowed = ['cover', 'contain'];
-  const file = `../data/${new Date().toJSON()}.png`;
-
+export default function(url, constraint = 'contain', ratio) {
+  const fits = ['cover', 'contain'];
   const w = 625;
   const h = 400;
 
-  let image_file_b64 = '';
-  let image_url = '';
+  const fit = fits.find(a => a === constraint) || 'contain';
 
-  const fit = allowed.find(a => a === constraint) || 'contain';
+  return new Promise((resolve, reject) => {
+    Jimp.read(url)
+      .then(img => {
+        if (img.bitmap.width <= w && img.bitmap.height <= h) {
+          resolve({ image_url: url });
+        }
 
-  await Jimp.read(url)
-    .then(img => {
-      if (img.bitmap.width <= w && img.bitmap.height <= h) {
-        core.log.info(`image is within ${w}x${h}, returning original URL`);
-
-        image_url = url;
-      } else {
         fit === 'contain' ? img.contain(w, h) : img.cover(w, h);
 
-        img.getBase64(Jimp.MIME_PNG, (err, b64) => {
-          if (err) return core.log.error(err);
+        img.getBase64(Jimp.MIME_PNG, (err, image_file_b64) => {
+          if (err) reject(err);
 
-          image_file_b64 = b64;
+          resolve({ image_file_b64 });
         });
-      }
-    })
-    .catch(e => core.log.error(e));
-
-  return { image_file_b64, image_url };
+      })
+      .catch(e => core.log.error(e));
+  });
 }
