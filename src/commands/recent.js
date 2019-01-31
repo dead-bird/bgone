2043206987addb7f74e40d01af822cd74f451b14
DaveP <dave@wearecrystal.uk>
2019-01-31 23:26:12 +0000

    Safer `recent` function

--- src/commands/recent.js
+++ src/commands/recent.js
@@ -1,4 +1,5 @@
 import Command from '../modules/Command';
+import core from '../modules/core';
 
 export default new Command({
   name: 'Recent',
@@ -7,13 +8,18 @@ export default new Command({
   args: [],
 
   run: msg =>
-    new Promise(resolve => {
-      msg.channel.fetchMessages({ limit: 20 }).then(messages => {
-        resolve(
-          messages
-            .find(m => m.attachments.array().length > 0)
-            .attachments.first().url
-        );
-      });
+    new Promise((resolve, reject) => {
+      msg.channel
+        .fetchMessages({ limit: 20 })
+        .then(messages => {
+          let message = messages.find(m => m.attachments.array().length > 0);
+
+          if (message) {
+            resolve(message.attachments.first().url);
+          }
+
+          reject(`Couldn't find any recent attachments 😭`);
+        })
+        .catch(e => core.log.error(e));
     }),
 });
