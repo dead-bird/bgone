21641f0dcb00797437ee552721819fbd09193db0
DaveP <dave@wearecrystal.uk>
2019-02-03 15:13:49 +0000

    Save image > base64 encode

--- src/modules/process.js
+++ src/modules/process.js
@@ -2,6 +2,7 @@ import core from './core';
 import Jimp from 'jimp';
 
 export default function(url, constraint = 'contain', ratio) {
+  const file = `${new Date().toJSON()}.png`;
   const fits = ['cover', 'contain'];
   const w = 625;
   const h = 400;
@@ -12,16 +13,21 @@ export default function(url, constraint = 'contain', ratio) {
     Jimp.read(url)
       .then(img => {
         if (img.bitmap.width <= w && img.bitmap.height <= h) {
-          resolve({ image_url: url });
+          resolve({ url });
         }
 
         fit === 'contain' ? img.contain(w, h) : img.cover(w, h);
 
-        img.getBase64(Jimp.MIME_PNG, (err, image_file_b64) => {
-          if (err) reject(err);
+        img
+          .writeAsync(`./src/data/${file}`)
+          .then(() => resolve({ file }))
+          .catch(e => core.log.error(e));
 
-          resolve({ image_file_b64 });
-        });
+        // img.getBase64(Jimp.MIME_PNG, (err, image_file_b64) => {
+        //   if (err) reject(err);
+
+        //   resolve({ image_file_b64 });
+        // });
       })
       .catch(e => core.log.error(e));
   });
