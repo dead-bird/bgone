f5c6404048dda936820b660bf3cb185ccc6677ae
DaveP <dave@wearecrystal.uk>
2019-02-04 23:10:23 +0000

    Set activity

--- src/bgone.js
+++ src/bgone.js
@@ -7,7 +7,14 @@ const bot = new Client();
 
 bot.login(process.env.TOKEN).catch(e => core.log.error(e));
 
-bot.on('ready', () => core.log.info('ready'));
+bot.on('ready', () => {
+  bot.user.setActivity('Image backgrounds removed by remove.bg', {
+    type: 'Playing',
+  });
+
+  core.log.info('ready');
+});
+
 bot.on('error', e => core.log.error(e));
 bot.on('warn', w => core.log.warn(w));
 bot.on('message', m => handler(m));
