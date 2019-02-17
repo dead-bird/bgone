d83f4ed1ecc1ebd7c980442466916cf028a5176b
DaveP <dave@wearecrystal.uk>
2019-02-17 23:20:19 +0000

    Started cognitive module

---
+++ src/modules/cognitive.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const api =
+  'https://uksouth.api.cognitive.microsoft.com/face/v1.0?returnFaceId=false&returnFaceLandmarks=true&returnFaceAttributes=headPose';
+
+export default url =>
+  new Promise((resolve, reject) => {
+    // axios.post(api, )
+  });
