adf4861f4b1c3799c20d9df85d1dfcb22736ef8d
DaveP <dave@wearecrystal.uk>
2019-02-17 00:12:18 +0000

    Moved process function into an object for future functionality 👀

--- src/modules/process.js
+++ src/modules/process.js
@@ -1,24 +1,30 @@
 import Jimp from 'jimp';
 
-export default function(url) {
-  const file = `./src/data/in-${new Date().toJSON()}.png`;
-  const origin = 'jimp';
-  const w = 625;
-  const h = 400;
+export default {
+  resize: url => {
+    const file = `./src/data/in-${new Date().toJSON()}.png`;
+    const origin = 'jimp';
+    const w = 625;
+    const h = 400;
 
-  return new Promise((resolve, reject) => {
-    Jimp.read(url)
-      .then(img => {
-        if (img.bitmap.width <= w && img.bitmap.height <= h) {
-          resolve({ url });
-        } else {
-          img
-            .contain(w, h)
-            .writeAsync(file)
-            .then(() => resolve({ file }))
-            .catch(e => reject({ type: 'reply', msg: e.message, origin }));
-        }
-      })
-      .catch(e => reject({ type: 'reply', msg: e.message, origin }));
-  });
-}
+    return new Promise((resolve, reject) => {
+      Jimp.read(url)
+        .then(img => {
+          if (img.bitmap.width <= w && img.bitmap.height <= h) {
+            resolve({ url });
+          } else {
+            img
+              .contain(w, h)
+              .writeAsync(file)
+              .then(() => resolve({ file }))
+              .catch(e => reject({ type: 'reply', msg: e.message, origin }));
+          }
+        })
+        .catch(e => reject({ type: 'reply', msg: e.message, origin }));
+    });
+  },
+
+  eyes() {
+    console.log('💧 💧');
+  },
+};
