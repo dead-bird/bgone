73e1d58bcf855f629f8ef8729ede4c9a0b36552d
DaveP <dave@wearecrystal.uk>
2019-05-28 21:56:13 +0100

    More WIP Remove

--- src/modules/remove.js
+++ src/modules/remove.js
@@ -6,21 +6,24 @@ export default {
   bg: data =>
     new Promise((resolve, reject) => {
       const destination = `./src/data/out-${new Date().toJSON()}.png`;
+      const config = { size: 'medium', destination };
       const origin = 'remove.bg';
-
-      const config = {
-        apiKey: process.env.REMOVD_API_KEY,
-        size: 'regular',
-        destination,
-      };
 
       if (data.file) {
         file({ source: data.file, ...config })
-          .then(item => resolve(destination, item))
+          .then(item => {
+            if (item.error) throw item;
+            console.log(item);
+            resolve(destination, item);
+          })
           .catch(e => reject({ type: 'reply', msg: e.error, origin }));
       } else {
         url({ source: data.url, ...config })
-          .then(item => resolve(destination, item))
+          .then(item => {
+            if (item.error) throw item;
+            console.log(item);
+            resolve(destination, item);
+          })
           .catch(e => reject({ type: 'reply', msg: e.error, origin }));
       }
     }),
